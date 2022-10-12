4326346d65795976e6566a966cfb65137e43ec2a
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-12 15:49:10 +0300

    logo redirect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,9 +7,15 @@ const NavBar = () => {
 	return (
 		<nav className={styles.navbar}>
 			<ul>
-				<li className={styles.title}>NavBar</li>
-				<li><Link to='/login'>Login</Link></li>
-				<li><Link to='signup'>Signup</Link></li>
+				<li className={styles.title}>
+					<Link to="/">NavBar</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+				<li>
+					<Link to="signup">Signup</Link>
+				</li>
 			</ul>
 		</nav>
 	);
