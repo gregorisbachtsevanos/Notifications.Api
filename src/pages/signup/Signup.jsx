915884153f168fcdb15705dd793c316fc0bf8772
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-12 15:48:50 +0300

    signup form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import styles from './Signup.module.css'
+import React, { useState } from 'react';
+import styles from './Signup.module.css';
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+	const [username, setUsername] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 
-export default Signup
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(username, email, password)
+	}
+
+	return (
+		<form className={styles['signup-form']} onSubmit={handleSubmit}>
+			<h2>Signup</h2>
+			<label>
+				<span>username:</span>
+				<input
+					type="text"
+					onChange={(e) => setUsername(e.target.value)}
+					value={username}
+					required
+				/>
+			</label>
+			<label>
+				<span>email:</span>
+				<input
+					type="text"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+					required
+				/>
+			</label>
+			<label>
+				<span>password:</span>
+				<input
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
+					required
+				/>
+			</label>
+			<button className='btn'>SignUp</button>
+		</form>
+	);
+};
+
+export default Signup;
