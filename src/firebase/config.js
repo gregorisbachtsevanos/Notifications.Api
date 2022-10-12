e0cf71493c3d91d3416cf0cf46ed7cbb6d5a47ef
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-12 16:41:13 +0300

    signup hook (untested)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyCBZheqHI46QVZMxhmBBAywAfS9tzd2gg0",
-   authDomain: "keep-me-on-budget.firebaseapp.com",
-   projectId: "keep-me-on-budget",
-   storageBucket: "keep-me-on-budget.appspot.com",
-   messagingSenderId: "628866111276",
-   appId: "1:628866111276:web:3314fe6d8236322e873e89"
- };
+	apiKey: 'AIzaSyCBZheqHI46QVZMxhmBBAywAfS9tzd2gg0',
+	authDomain: 'keep-me-on-budget.firebaseapp.com',
+	projectId: 'keep-me-on-budget',
+	storageBucket: 'keep-me-on-budget.appspot.com',
+	messagingSenderId: '628866111276',
+	appId: '1:628866111276:web:3314fe6d8236322e873e89',
+};
 
 // init firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,4 +17,6 @@ firebase.initializeApp(firebaseConfig);
 // init service
 const projectFirestore = firebase.firestore();
 
-export { projectFirestore }
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
