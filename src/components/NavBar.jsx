49d0808ba61a0135b6ff0f29fd1efbae460170e6
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-13 14:21:57 +0300

    Logout functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
-import styles from './NavBar.module.css';
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout'
+import styles from './NavBar.module.css';
+
 
 const NavBar = () => {
+	const { logout } = useLogout();
+
 	return (
 		<nav className={styles.navbar}>
 			<ul>
@@ -15,6 +18,9 @@ const NavBar = () => {
 				</li>
 				<li>
 					<Link to="signup">Signup</Link>
+				</li>
+				<li>
+					<Link to="/" onClick={logout}>Logout</Link>
 				</li>
 			</ul>
 		</nav>
