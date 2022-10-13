db18025c607841b45777613db0d87a513310d0c3
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-13 17:06:51 +0300

    showing user's context

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext.js';
+import { useLogout } from '../hooks/useLogout';
 import styles from './NavBar.module.css';
-
 
 const NavBar = () => {
 	const { logout } = useLogout();
-
+	const { user } = useAuthContext();
+	console.log(user);
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li className={styles.title}>
 					<Link to="/">NavBar</Link>
 				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="signup">Signup</Link>
-				</li>
-				<li>
-              <button className="btn" onClick={logout}>Logout</button>
-            </li>
+				{!user ? (
+					<Fragment>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="signup">Signup</Link>
+						</li>
+					</Fragment>
+				) : (
+					<Fragment>
+						<li>hello, {user.displayName}</li>
+						<li>
+							<button className="btn" onClick={logout}>
+								Logout
+							</button>
+						</li>
+					</Fragment>
+				)}
 			</ul>
 		</nav>
 	);
