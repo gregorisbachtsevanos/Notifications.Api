04c0c1e7942624aea20ef08efdc4a9546b416d27
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-14 18:31:26 +0300

    auth credentials

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,13 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+import { projectAuth } from '../firebase/config';
 
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
 	switch (action.type) {
 		case 'LOGIN':
+			return { ...state, user: action.payload };
+		case 'AUTH_IS_READY':
 			return { ...state, user: action.payload };
 		case 'LOGOUT':
 			return { ...state, user: null };
@@ -16,7 +19,16 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(authReducer, {
 		user: null,
+		authIsReady: false,
 	});
+
+	useEffect(() => {
+		const unsub = projectAuth.onAuthStateChanged((user) => {
+			dispatch({ type: 'AUTH_IS_READY', payload: user });
+			// unsub()
+		});
+	}, []);
+
 	console.log('State: ', state);
 
 	return (
