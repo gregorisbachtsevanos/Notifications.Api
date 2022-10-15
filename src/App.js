297942ce7e007b3f22342bbb2f30cbe9292d3483
GregorisB <gregorbachts@gmail.com>
2022-10-15 21:00:37 +0300

    protect routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import { useAuthContext } from './hooks/useAuthContext';
 import Home from './pages/home/Home';
@@ -6,17 +6,19 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
 function App() {
-	const { authIsReady } = useAuthContext()
-
+	const { authIsReady, user } = useAuthContext()
 	return (
 		<div className="App">
 			{authIsReady && (
 				<BrowserRouter>
 					<NavBar />
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Signup />} />
+						<Route path="/">
+							{user && <Navigate to='/login' />}
+							{user && <Route index element={<Home />} />}
+						</Route>
+						{!user && <Route path="/login" element={<Login />} />}
+						{!user && <Route path="/signup" element={<Signup />} />}
 					</Routes>
 				</BrowserRouter>
 			)}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
 import App from './App';
