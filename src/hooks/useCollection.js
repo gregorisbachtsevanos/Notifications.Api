7c615b40e05d5e2b7d4cd5fe63158edc72825797
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-17 17:38:59 +0300

    make useCollection hook boiletplate

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { projectFirestore } from '../../firebase/config';
+
+export const useCollection = (collection) => {
+	const [error, setError] = useState(false);
+	const [isPending, setIsPending] = useState(false);
+	const [transactions, setTransactions] = useState([]);
+
+	useEffect(() => {
+		setIsPending(true);
+
+		const unsub = projectFirestore.collection('transactions').onSnapshot(
+			(snapshot) => {
+				if (snapshot.empty) {
+					setError('No transactions tom load');
+					setIsPending(false);
+				} else {
+					let result = [];
+					snapshot.docs.forEach((doc) => {
+						result.push({ id: doc.id, ...doc.data() });
+					});
+					setTransactions(result);
+					setIsPending(false);
+					setError(null);
+				}
+			},
+			(err) => {
+				setError(err.message);
+				setIsPending(false);
+			}
+		);
+      return () => unsub()
+	}, [collection]);
+
+   return { error , isPending, transactions}
+};
