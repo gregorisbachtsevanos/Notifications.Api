dda2786cb00e50af55478fadcaf42bf640af67ab
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2024-08-14 23:05:21 +0300

    Delete src directory

--- src/utils/PrivateRouter.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouter = ({ user }) => {
-	// console.log(user);
-	return user ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRouter;
--- src/utils/PublicRouter.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PublicRouter = ({ user }) => {
-	// console.log(user);
-	return user ? <Navigate to="/" /> : <Outlet />;
-};
-
-export default PublicRouter;
